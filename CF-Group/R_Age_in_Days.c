5b50af9bfcce4330f87861ccb31248229aac71af
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-13 21:37:42 +0600

    feat(CF-Group/Capital_Small_or_Digit.c): add solution using ASCII checks to determine if input is digit, capital letter, or small letter

--- CF-Group/R_Age_in_Days.c
+++ CF-Group/R_Age_in_Days.c
@@ -1,19 +1,17 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(){
-//     int day, month, year;
-//     scanf("%d", &day);
+int main(){
+    int day, month, year;
+    scanf("%d", &day);
 
-//     year = day/365;
-//     month = (day%365)/30; 
-//     day = (day%365) % 30;
+    year = day/365;
+    month = (day%365)/30; 
+    day = (day%365) % 30;
 
-//     printf("%d years\n%d months\n%d days", year, month, day);
-//     return 0;
-// }
+    printf("%d years\n%d months\n%d days", year, month, day);
+    return 0;
+}
 
 // year = day / 365;        // মোট দিনকে 365 দিয়ে ভাগ করলে কত বছর হবে
 // month = (day % 365) / 30; // বছর বাদ দিলে বাকি দিনগুলো থেকে কত মাস হবে
 // day = (day % 365) % 30;   // মাস বাদ দিলে বাকি দিনগুলো থেকে কত দিন হবে
-
-
