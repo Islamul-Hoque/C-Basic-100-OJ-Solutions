1bde7df1e0c388d0fced44c07ba3bc71c77702ec
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-15 00:13:37 +0600

    feat(AtCoder/Rated_for_Me.c): add solution to determine next contest (ABC, ARC, AGC) based on rating thresholds

---
+++ AtCoder/Rated_for_Me.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n<1200) printf("ABC");
+    else if(n>=1200 && n<2800) printf("ARC");
+    else printf("AGC");
+    return 0;
+}
