4ddf7a8a2f4b4b44cf95a368d4fda4f2844c7934
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-15 22:19:03 +0600

    feat(AtCoder/Multiple_of_2_and_N.c): add solution to find minimum positive integer divisible by both 2 and N

--- CF-Group/M_Capital_or_Small_or_Digit.c
+++ CF-Group/M_Capital_or_Small_or_Digit.c
@@ -1,18 +1,16 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     char x;
-//     scanf("%c", &x);
+int main() {
+    char x;
+    scanf("%c", &x);
 
-//     if(x>='0' && x<='9') printf("IS DIGIT\n");
-//     else{
-//         printf("ALPHA\n");
-//         if(x>='a' && x<='z') printf("IS SMALL\n");
-//         if(x>='A' && x<='Z') printf("IS CAPITAL\n");
-//     }
-//     return 0;
-// }
+    if(x>='0' && x<='9') printf("IS DIGIT\n");
+    else{
+        printf("ALPHA\n");
+        if(x>='a' && x<='z') printf("IS SMALL\n");
+        if(x>='A' && x<='Z') printf("IS CAPITAL\n");
+    }
+    return 0;
+}
 
-// git add .
-// git commit -m "feat(CF-Group/Capital_Small_or_Digit.c): add solution using ASCII checks to determine if input is digit, capital letter, or small letter"
-// git push
+
