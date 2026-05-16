3b0bc82c48fb9b0aaf023c9d17f804a874d1fee9
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-16 18:51:30 +0600

    feat(MarisaOJ/Hard_Compare.c): add solution to compare A^B and C^D using logarithms to avoid overflow

---
+++ Codeforces/Z_Hard_Compare.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    long long int a, b, c, d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+
+    if(b*log(a)  > d*log(c)) printf("YES");
+    else printf("NO");
+
+    return 0;
+}
