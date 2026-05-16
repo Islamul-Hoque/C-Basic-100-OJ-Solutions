1650c8278ba01bfbe25a3399ab0b5a2e304ed9a3
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-17 01:49:34 +0600

    feat(Age.c): add age comparison logic with year→month→day nested conditions

---
+++ C-Program/Age.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, x, y, z;  // day month, year || year → month → day
+    scanf("%d %d %d %d %d %d", &a, &b, &c, &x, &y, &z);
+
+    if(c<z) printf("1"); // year
+    else if(c>z) printf("2");
+    else {
+        if(b<y) printf("1");  // month
+        else if(b>y) printf("2");
+        else{
+            if(a<x) printf("1"); // day
+            else printf("2");
+        }
+    }
+    return 0;
+}
