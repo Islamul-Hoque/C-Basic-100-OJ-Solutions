802b4b08d995890410999b596886e07504ad5a9d
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-19 17:15:56 +0600

    feat(AtCoder/Addition_and_Subtraction_Easy.c): add solution to evaluate formula A op B where op is + or -

---
+++ AtCoder/Addition_and_Subtraction_Easy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char operator;
+    scanf("%d %c %d", &a, &operator, &b);
+
+    if(operator == '+') printf("%d\n", a + b);
+    else if(operator == '-') printf("%d\n", a - b);
+
+    return 0;
+}
+
